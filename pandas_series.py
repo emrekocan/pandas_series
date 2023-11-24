3e4f00b2e6264a66091662536c7b95d1bd6ba449
Emre Koçan <emre_kocan9@hotmail.com>
2023-11-24 18:16:01 +0300

    pandas_series
    use of pandas series

---
+++ pandas_series.py
@@ -0,0 +1,44 @@
+import pandas as pd 
+#import numpy as np
+
+#data
+#numbers = [20,30,40,50]
+#letters = ['a','b','c', 20]
+#scalar = 5
+#dict = {'a':10,'b':20,'c':30,'d':40}
+#random_numbers = np.random.randint(10,100,6)
+
+
+
+
+
+#pandas_series = pd.Series(numbers)
+#pandas_series = pd.Series(letters)
+#pandas_series = pd.Series(scalar, [0,1,2,3,4])
+#pandas_series = pd.Series(dict)
+#pandas_series = pd.Series(random_numbers)
+#pandas_series = pd.Series([20,30,40,50], ['a','b','c','d'])
+
+
+
+#result = pandas_series[-1]
+#result = pandas_series[-2:]
+#result = pandas_series['a']
+#result = pandas_series + pandas_series
+#result = pandas_series + pandas_series*2
+#result = pandas_series + 50
+#result = pandas_series >=40
+#result = pandas_series % 2 == 0
+
+
+
+
+#print(pandas_series)
+#print(result)
+
+#örnek
+#opel2018 = pd.Series([20,30,40,10],["astra","corsa","insignia","mokka"])
+#opel2019 = pd.Series([40,30,20,60],["astra","corsa","grandland","mokka"])
+
+#total = opel2018 + opel2019
+#print(total["astra"])
